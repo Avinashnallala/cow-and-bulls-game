bd37b59891624ee3069cf96d4c7589e0f481ba5f
aviansh <anallala@clarku.edu>
2025-11-03 18:32:50 -0500

    the complete code is updated

--- cow_bull.py
+++ cow_bull.py
@@ -1,1 +1,25 @@
-hi
+import random as rd
+s=[]
+cows=0
+bulls=0
+for i in range(0,4):
+    k=rd.randint(0,9)
+    s.append(str(k))
+k=''.join(s)
+m=list(input('enter the four digit number'))
+for i in range(0,4):
+    if s[i]==m[i]:
+        cows+=1
+    else:
+        bulls+=1
+print(f'the cows score is {cows} and bulls score is {bulls}')
+if cows>bulls:
+    print(f"cows has higher score {cows} and cows wins!!!!")
+elif cows==bulls:
+    print('your both scores are equal')
+else:
+    print(f'the bulls score is higher {bulls},bulls win!!!!!!')
+
+
+print(s)
+print(m)
